1e7b1d8eb8273679aed0a90d0b77711a64cb7ad2
Abid Adhikari <70445257+abidadhikari@users.noreply.github.com>
2020-10-07 09:08:23 +0545

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,8 +1,43 @@
 <?php
-$name=$_POST['name'];
-$email=$_POST['email'];
-$message=$_POST['textarea'];
+$insert = false;
+if(isset($_POST['name'])){
+    // Set connection variables
+    $server = "localhost";   //default for xampp server
+    $username = "root";
+    $password = "";
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
+
+    // Check for connection success
+    if(!$con){
+        die("Database connection failed" . mysqli_connect_error());
+    }
+    // echo "Success connecting to the db";
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $topic=$_POST['title'];
+    $message=$_POST['textarea'];
+    $sql = "INSERT INTO `CONTACTUSFORM`.`USERMESSAGES`(`name`,`email`,`topic`,`message`)VALUES($name,$email,$topic,$message)";
+    // echo $sql;
+
+    // Execute the query
+    if($con->query($sql) == true){
+        // echo "Successfully inserted";
+
+        // Flag for successful insertion
+        $insert = true;
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+
+    // Close the database connection
+    $con->close();
+}
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,5 +54,8 @@ $message=$_POST['textarea'];
  <p>WE WILL SOON REPLY YOU AT:</p><?php echo $_POST['email'] ?>
 </div>
 <h4>Created By Abid Adhikari</h4>
+
+<!-- INSERT INTO `usermessages` (`name`, `email`, `topic`, `message`) VALUES ('test bahadur', 'test@bahadur.com', 'jpt lang', 'k saro ramri vako?'); -->
+
 </body>
 </html>
